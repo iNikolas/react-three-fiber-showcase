f2dc72ae46b71f67953746aa5c306a90db3d7d71
iNikolas <realkolos1@gmail.com>
2023-07-27 20:42:56 +0300

    typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,70 @@
 import React from 'react'
 import * as THREE from 'three'
 import { Stats, useScroll, Loader, useTexture, ScrollControls, Sky, Center } from '@react-three/drei'
-import { Canvas, useLoader, useFrame } from '@react-three/fiber'
+import { Canvas, useLoader, useFrame, extend, ReactThreeFiber } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { getProject, val } from '@theatre/core'
 import { SheetProvider, PerspectiveCamera, useCurrentSheet } from '@theatre/r3f'
 
 import animatedCameraState from './animated-camera-state.json'
 
+extend({ Line_: THREE.Line })
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      line_: ReactThreeFiber.Object3DNode<THREE.Line, typeof THREE.Line>
+    }
+  }
+}
+
 const humanSkinColor = '#FC9'
 const floorSize = 1000
-const sunPosition = [10, 60, 200]
+const sunPosition: [number, number, number] = [10, 60, 200]
 const crosshairThreshold = 40
 const crosshairColor = 'orange'
 const crosshairWidth = 0.1
 const crosshairPadding = 0.3
 const crosshairPeripheryLineLength = 10000
 
-function Crosshair({ target }) {
-  const crossHairRef = React.useRef(null)
+function assertIsMesh(object: unknown): asserts object is THREE.Mesh {
+  if ((object as any).type !== 'Mesh') {
+    throw new Error('Provided object is not of Mesh type')
+  }
+}
+
+function Crosshair({ target }: { target: THREE.Group }) {
+  const crossHairRef = React.useRef<THREE.Mesh | null>(null)
 
   const targetMesh = target.children[0]
-  const targetMeshHeight =
-    (targetMesh.geometry.boundingBox.max.z - targetMesh.geometry.boundingBox.min.z) * targetMesh.scale.z
-  const crosshairSize =
-    Math.max(targetMesh.geometry.boundingSphere.radius * targetMesh.scale.z, targetMeshHeight) + crosshairPadding
+  assertIsMesh(targetMesh)
+
+  const targetMeshHeight = React.useMemo(() => {
+    if (!targetMesh.geometry.boundingBox) {
+      targetMesh.geometry.computeBoundingBox()
+    }
+
+    if (!targetMesh.geometry.boundingBox) {
+      throw new Error('Bounding Box for a Mesh was not computed')
+    }
+
+    return (targetMesh.geometry.boundingBox.max.z - targetMesh.geometry.boundingBox.min.z) * targetMesh.scale.z
+  }, [targetMesh])
+
+  const crosshairSize = React.useMemo(() => {
+    if (!targetMesh.geometry.boundingSphere) {
+      targetMesh.geometry.computeBoundingSphere()
+    }
+
+    if (!targetMesh.geometry.boundingSphere) {
+      throw new Error('Bounding Sphere for a Mesh was not computed')
+    }
+
+    return Math.max(targetMesh.geometry.boundingSphere.radius * targetMesh.scale.z, targetMeshHeight) + crosshairPadding
+  }, [targetMesh, targetMeshHeight])
 
   useFrame(({ camera }) => {
-    if (crossHairRef) {
+    if (crossHairRef && crossHairRef.current) {
       crossHairRef.current.rotation.x = camera.rotation.x
       crossHairRef.current.rotation.y = camera.rotation.y
       crossHairRef.current.rotation.z = camera.rotation.z
@@ -65,8 +102,7 @@ function Crosshair({ target }) {
         target.position.y + crosshairWidth * 2 + crosshairPadding + targetMeshHeight / 2,
         target.position.z
       ]}
-      geometry={geometry}
-    >
+      geometry={geometry}>
       <meshBasicMaterial color={crosshairColor} />
       <CrosshairPeripheryLines crosshairSize={crosshairSize} topOffset={crosshairWidth * 2} />
     </mesh>
@@ -84,28 +120,28 @@ horizontalPoints.push(new THREE.Vector3(crosshairPeripheryLineLength, 0, 0))
 const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(verticalPoints)
 const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(horizontalPoints)
 
-function CrosshairPeripheryLines({ crosshairSize, topOffset }) {
+function CrosshairPeripheryLines({ crosshairSize, topOffset }: { crosshairSize: number; topOffset: number }) {
   return (
     <>
       <Center position={[0, crosshairSize / 2 - topOffset, 0]} top>
-        <line geometry={verticalLineGeometry}>
+        <line_ geometry={verticalLineGeometry}>
           <lineBasicMaterial attach="material" color={crosshairColor} />
-        </line>
+        </line_>
       </Center>
       <Center position={[0, -crosshairSize / 2, 0]} bottom>
-        <line geometry={verticalLineGeometry}>
+        <line_ geometry={verticalLineGeometry}>
           <lineBasicMaterial attach="material" color={crosshairColor} />
-        </line>
+        </line_>
       </Center>
       <Center position={[-crosshairSize / 2, 0, 0]} left>
-        <line geometry={horizontalLineGeometry}>
+        <line_ geometry={horizontalLineGeometry}>
           <lineBasicMaterial attach="material" color={crosshairColor} />
-        </line>
+        </line_>
       </Center>
       <Center position={[crosshairSize / 2, 0, 0]} right>
-        <line geometry={horizontalLineGeometry}>
+        <line_ geometry={horizontalLineGeometry}>
           <lineBasicMaterial attach="material" color={crosshairColor} />
-        </line>
+        </line_>
       </Center>
     </>
   )
@@ -129,7 +165,7 @@ function Floor() {
 }
 
 function Model() {
-  const [meshToOutline, setMeshToOutline] = React.useState(null)
+  const [meshToOutline, setMeshToOutline] = React.useState<THREE.Group | null>(null)
 
   const { scene: human } = useLoader(GLTFLoader, './models/human.glb')
   const { scene: cat } = useLoader(GLTFLoader, './models/cat.glb')
@@ -143,12 +179,12 @@ function Model() {
       sheet.sequence.position = scroll.offset * sequenceLength
     }
 
-    const cameraDistances = [
+    const cameraDistances: Array<[THREE.Group, number]> = [
       [human, camera.position.distanceTo(human.position)],
       [cat, camera.position.distanceTo(cat.position)]
     ]
 
-    const newMeshToOutline = cameraDistances.reduce(
+    const newMeshToOutline = cameraDistances.reduce<[null | THREE.Group, number]>(
       ([accMesh, accDistance], [mesh, distance]) =>
         accDistance < distance ? [accMesh, accDistance] : [mesh, distance],
       [null, crosshairThreshold]
@@ -202,7 +238,14 @@ export default function App() {
               <Floor />
               <Model />
             </React.Suspense>
-            <PerspectiveCamera theatreKey="Camera" makeDefault position={[0, 0, 0]} />
+            <PerspectiveCamera
+              theatreKey="Camera"
+              makeDefault
+              position={[0, 0, 0]}
+              attachArray={undefined}
+              attachObject={undefined}
+              attachFns={undefined}
+            />
             <Stats />
           </SheetProvider>
         </ScrollControls>
